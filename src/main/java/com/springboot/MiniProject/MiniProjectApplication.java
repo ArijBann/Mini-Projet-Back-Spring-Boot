35d7603553afe7baf9b674cb092c404cfbda3b20
ArijBann <arijbannour45@gmail.com>
2024-03-20 11:42:55 +0100

    first commit

---
+++ src/main/java/com/springboot/MiniProject/MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.MiniProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/MiniProject/MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.MiniProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
