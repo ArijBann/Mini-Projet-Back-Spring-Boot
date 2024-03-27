7ff3a0fef0bbc3328994c6162bb8db19346ea55b
ArijBann <arijbannour45@gmail.com>
2024-03-27 17:06:36 +0100

    Add EtudRepository

--- src/main/java/com/springboot/MiniProject/repository/EtudRepository.java
+++ src/main/java/com/springboot/MiniProject/repository/EtudRepository.java
@@ -5,6 +5,11 @@ import com.springboot.MiniProject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface EtudRepository extends JpaRepository<Etudiant,Integer> {
+    Etudiant findEtudiantByNum_inscri(int numInscrit);
+    List <Etudiant> findEtudiantByGroupe(int idGroupe);
 }
