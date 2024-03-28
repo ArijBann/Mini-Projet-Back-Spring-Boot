29a2008cd65758ed9a63bf3272c4e9daecb93f89
ArijBann <arijbannour45@gmail.com>
2024-03-28 23:28:26 +0100

    add the enseignant Update numTel

--- src/main/java/com/springboot/MiniProject/serivce/EnseignantService.java
+++ src/main/java/com/springboot/MiniProject/serivce/EnseignantService.java
@@ -1,9 +1,12 @@
 package com.springboot.MiniProject.serivce;
 
+import com.springboot.MiniProject.dto.EnseignantDTO;
+import com.springboot.MiniProject.dto.EtudiantDTO;
 import com.springboot.MiniProject.dto.UserEnseigantDTO;
 import com.springboot.MiniProject.entity.Enseignant;
 import com.springboot.MiniProject.entity.User;
 import com.springboot.MiniProject.repository.EnseignantRepository;
+import com.springboot.MiniProject.repository.EtudRepository;
 import com.springboot.MiniProject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,25 +17,15 @@ import java.util.stream.Collectors;
 
 @Service
 public class EnseignantService {
-   /* @Autowired
-    private EnseignantRepository enseignantRepository;
     @Autowired
-    private  UserRepository userRepository;
-    public List<UserEnseigantDTO> getAllEnseignants() {
-        List<Enseignant> enseignants = enseignantRepository.findAll();
-        return enseignants.stream()
-                .map(enseignant -> new UserEnseigantDTO(userRepository.findUserByEnseignantId(enseignant.getId()).orElse(null), enseignant))
-                .collect(Collectors.toList());
+    EnseignantRepository enseignantRepository;
+    @Autowired
+    UserRepository userRepository;
+    public EnseignantDTO updateEtudiantInfo(EnseignantDTO enseignantDTO){
+        User userExist = userRepository.findUserByEnseignantId(enseignantDTO.getIdEnseignant());
+        userExist.setNumtel(enseignantDTO.getNumtel());
+        userRepository.save(userExist);
+        return enseignantDTO;
     }
-
-    public UserEnseigantDTO getEnseignantByEmail(String email) {
-        Optional<User> user = userRepository.findByEmail(email);
-        if (user.isPresent() && user.get().getEnseignant() != null) {
-            Enseignant enseignant = user.get().getEnseignant();
-            return new UserEnseigantDTO(user.get(), enseignant);
-        } else {
-            return null;
-        }
-    }*/
 
 }
