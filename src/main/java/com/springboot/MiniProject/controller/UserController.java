ef3567d9d2832ad637915994b2ee0ba12fdc0e49
ArijBann <arijbannour45@gmail.com>
2024-03-31 14:17:27 +0200

    delete changePassword from userController

--- src/main/java/com/springboot/MiniProject/controller/UserController.java
+++ src/main/java/com/springboot/MiniProject/controller/UserController.java
@@ -1,5 +1,4 @@
 package com.springboot.MiniProject.controller;
-
 import com.springboot.MiniProject.dto.*;
 import com.springboot.MiniProject.entity.RefreshToken;
 import com.springboot.MiniProject.entity.Enseignant;
@@ -8,12 +7,15 @@ import com.springboot.MiniProject.serivce.JwtService;
 import com.springboot.MiniProject.serivce.UserService;
 import com.springboot.MiniProject.serivce.RefreshTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/issatso")
@@ -95,5 +97,6 @@ public class UserController {
                 }).orElseThrow(() -> new RuntimeException(
                         "Refresh token is not in database!"));
     }
+
 
 }
